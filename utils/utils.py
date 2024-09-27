5ca7f8ca4a023cfffe1e9d2f32dfc11a9a5b1854
harakiriOP <salvadorhidalgo100@gmail.com>
2024-09-26 21:41:04 -0500

    readme

--- utils/utils.py
+++ utils/utils.py
@@ -1,4 +1,4 @@
-MATRIZ_INTERFAZ = "matrices/matriz3.txt"
-MATRIZ_BUSQUEDA = "matrices/matriz3.txt"
+MATRIZ_INTERFAZ = "matrices/matriz2.txt"
+MATRIZ_BUSQUEDA = "matrices/matriz2.txt"
 NODO_INICIO = 'E'
 NODO_META = 'P'
